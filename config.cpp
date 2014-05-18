f912f079d1deb64728144f782f533b68423b0f93
HopeJohnson <59Hopejohnson@gmail.com>
2014-05-18 00:08:43 -0700

    Initial upload

---
+++ config.cpp
@@ -0,0 +1,28 @@
+class CfgPatches {
+  class HJ_Buds {
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {};
+    version = 0.1;
+    author[] = {"HopeJ"};
+    authorUrl = "";
+  };
+};
+
+
+class cfgweapons 
+{
+class optic_hamr;
+
+class i_earbuds_f : optic_hamr
+	{
+	scope = 2;
+	model = "\A3\weapons_F\ammo\mag_univ.p3d";
+	displayname = "Protective Ear Plugs";
+	descriptionShort = "";
+	picture = "\EarBuds\ui_plugs.paa";	
+	
+	};
+
+};
